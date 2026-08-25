d2efbd133e94a43f54241c0c2e6e37ad7f4ae8f1
hqcoder05 <hoangquocfw@gmail.com>
2026-08-25 20:02:26 +0700

    chore: clean up temp scripts

--- shiftsync-backend/fix_employment_mapper.ps1
+++
@@ -1,5 +0,0 @@
-$file = "src\main\java\com\shiftsync\employment\mapper\EmploymentMapper.java"
-$content = Get-Content $file -Raw
-$content = $content -replace "import com.shiftsync.employment.dto.EmploymentDTO;`r?`n", "import com.shiftsync.employment.dto.EmploymentDTO;`r`nimport com.shiftsync.employment.dto.ContractTypeDTO;"
-$content = $content -replace ".employmentType\(employment.getEmploymentType\(\)\)", ".contractType(employment.getContractType() != null ? ContractTypeDTO.builder().id(employment.getContractType().getId()).name(employment.getContractType().getName()).maxWeeklyHours(employment.getContractType().getMaxWeeklyHours()).otMultiplier(employment.getContractType().getOtMultiplier()).defaultHourlyRate(employment.getContractType().getDefaultHourlyRate()).build() : null)"
-Set-Content $file -Value $content
