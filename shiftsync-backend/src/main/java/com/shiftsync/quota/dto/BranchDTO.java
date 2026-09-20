1b1c523a993bb1bf9098166501cee45793f528f4
hqcoder05 <hoangquocfw@gmail.com>
2026-09-20 15:33:01 +0700

    Cập nhật BranchDTO.java

--- shiftsync-backend/src/main/java/com/shiftsync/quota/dto/BranchDTO.java
+++ shiftsync-backend/src/main/java/com/shiftsync/quota/dto/BranchDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalTime;
 import java.util.UUID;
 
 @Data
@@ -17,4 +18,7 @@ public class BranchDTO {
     private String code;
     private String address;
     private String format;
+    private LocalTime openTime;
+    private LocalTime closeTime;
 }
+
